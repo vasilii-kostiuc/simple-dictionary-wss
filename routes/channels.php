23fa775ee996323e7e3ba5c91561c40946357546
vasea <vasilii.costiuc@proton.me>
2025-10-27 17:33:32 +0200

    Update dependencies in composer.json and composer.lock

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Support\Facades\Http;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+Broadcast::channel('training.{id}', function (int $id, \Psr\Http\Client\ClientInterface $client) {
+    $token = request()->bearerToken();
+    $response = $client->get('/api/user', [
+        'headers' => [
+            'Authorization' => 'Bearer ' . $token,
+            'Accept' => 'application/json',
+        ],
+    ]);
+
+    if($response->getStatusCode() !== 200) {
+        return false;
+    }
+
+    $user = json_decode($response->getBody(), true);
+
+    return true;
 });
