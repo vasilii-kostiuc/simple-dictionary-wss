9aeb86b89fea49396c74a6f63995d97cd4fbb9b4
vasea <vasilii.costiuc@proton.me>
2026-02-03 17:28:25 +0200

    deployment to hetzner with github actions

--- _docker/wss_app/entrypoint.sh
+++ _docker/wss_app/entrypoint.sh
@@ -7,6 +7,11 @@ until nc -z redis 6379 && nc -z mongo 27017; do
   sleep 1
 done
 
+# Create storage directories
+mkdir -p storage/framework/{sessions,views,cache} storage/logs
+chown -R www-data:www-data storage bootstrap/cache
+chmod -R 775 storage bootstrap/cache
+
 # Применяем миграции, если нужно
 php artisan migrate --force || true
 
